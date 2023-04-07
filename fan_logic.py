a497cdecb296389e61477b4c1dbf6e1caf3cafac
hays13 <54752023+hays13@users.noreply.github.com>
2023-04-07 09:45:15 -0400

    Update fan_logic.py
    updated url formats to match server

--- fan_logic.py
+++ fan_logic.py
@@ -1,21 +1,41 @@
-import requests, datetime, kivy
-from machine import Timer
+import requests, datetime, dht
+from machine import Timer, Pin
 from kivy.network.urlrequest import UrlRequest
 
 def set_fan_speed(fan_speed):
     # send corresponding fan speed to power relay
     # push current fan_speed value to server
-    url = 'http://172.20.10.8/fan_speed={}/'.format(fan_speed)
+    SERVER = 'http://172.20.10.8'
+    result = UrlRequest.get(SERVER)
+    resultList = result.split("/")
+    
+    fan_speed_server = [s.split('=')[1] for s in resultList if "speed=" in s][0]
+    if fan_speed:
+        if fan_speed_server > 0:
+            url = '{}/speed={}/'.format(SERVER, fan_speed_server)
+        else:
+            url = '{}/speed={}/'.format(SERVER, fan_speed)
+    else:
+        url = '{}/speed={}/'.format(SERVER, fan_speed)
+    # posts to server
     try:
         UrlRequest.post(url)
     except:
-        print("cannot connect to server")
+        print("Cannot connect to server")
 
 def get_time():
     t = datetime.datetime.now()
     hour = t.hour
     minute = t.minute
     return hour, minute
+
+def read_sensor(sensor):
+    sensor.measure()
+    temperature = sensor.temperature()
+    temperature = temperature * 1.8 + 32 #converts from C to F
+    humidity = sensor.humidity()
+    
+    return temperature, humidity
 
 def read_sensor(timer):
     #################################################
@@ -27,23 +47,22 @@ def read_sensor(timer):
     SERVER = 'http://172.20.10.8'
     result = requests.get(SERVER)
     resultList = result.split("/")
+    sensor = dht.DHT22(Pin(14, Pin.IN, Pin.PULL_UP))
 
-    # load currTemp from sensor
-    # currTemp = load_from_sensor()
+    # load currTemp and currHumd from sensor
+    currTemp, currHumd = read_sensor(sensor)
     # load maxTemp from server
-    maxTemp = [s.split('=')[1] for s in resultList if "max_temp=" in s]
+    maxTemp = [s.split('=')[1] for s in resultList if "max_temp=" in s][0]
     # load minTemp from server
-    minTemp = [s.split('=')[1] for s in resultList if "min_temp=" in s]
+    minTemp = [s.split('=')[1] for s in resultList if "min_temp=" in s][0]
     
-    # load currHumd from sensor
-    # currHumd = load_from_sensor()
     # load maxHumd from server
-    maxHumd = [s.split('=')[1] for s in resultList if "max_humidity=" in s]
+    maxHumd = [s.split('=')[1] for s in resultList if "max_humidity=" in s][0]
     # load minHumd from server
-    minHumd = [s.split('=')[1] for s in resultList if "min_humidity=" in s]
+    minHumd = [s.split('=')[1] for s in resultList if "min_humidity=" in s][0]
     
     # load list of schedules
-    schedule = [s.split('=')[1] for s in resultList if "schedule=" in s]
+    schedule = [s.split('=')[1] for s in resultList if "schedule=" in s] # [[start_time, end_time], ...]
     # load current time
     curr_hour, curr_min = get_time()
     curr_time = curr_hour * 60 + curr_min
@@ -54,20 +73,20 @@ def read_sensor(timer):
     if currTemp > maxTemp:
         # turn on fan
         on_counter += 1
-        # send fan speed to server
+    
     elif currTemp < minTemp:
         # turn off fan
         pass
-        # send fan speed to server
+        
     if currHumd > maxHumd:
         # turn on fan
         on_counter += 1
-        # send fan speed to server
+        
     elif currHumd < minHumd:
         # turn off fan
         pass
-        # send fan speed to server
-    # unsure of format of schedules: assuming an input of a 2D array schedule = [[start, end], ...]  [11:15, 13:30]
+        
+    # assuming an input of a 2D array schedule = [[start, end], ...] (ie. [11:15, 13:30])
     if len(schedule) > 0: # if there is a schedule set
         fan_counter = 0
         for s in range(len(schedule)):
@@ -89,31 +108,16 @@ def read_sensor(timer):
         if fan_counter > 0:
             # turn on fan
             on_counter += 1
-            # send fan speed to server
-        else:
-            # turn off fan
-            pass
-            # send fan speed to server
             
     if on_counter > 0:
+        # turns on fan
         set_fan_speed(50)
         return
     else:
+        # turns of fan
         set_fan_speed(0)
         return
 
-def read_fundamentals(timer):
-    # requesting status fan speed
-    # read fan_speed from server, then send the corresponding speed to the relay (calling the corresponding mode)
-    SERVER = 'http://172.20.10.8'
-    result = UrlRequest.get(SERVER)
-    resultList = result.split("/")
-    
-    fan_speed = [s.split('=')[1] for s in resultList if "speed=" in s]
-    set_fan_speed(fan_speed)
-
 if __name__ == "__main__":
     T0 = Timer(0)
-    T1 = Timer(1)
     T0.init(period=15000, mode=Timer.PERIODIC, callback=read_sensor) #runs every 15 seconds
-    T1.init(period=1000, mode=Timer.PERIODIC, callback=read_fundamentals) #runs every 1 second
